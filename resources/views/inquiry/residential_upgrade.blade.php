820c926daa4de472c9261dbe93050fe2725e9295
Hp <aljonrojas01@gmail.com>
2026-04-16 09:21:39 +0800

    new

--- resources/views/inquiry/residential_upgrade.blade.php
+++ resources/views/inquiry/residential_upgrade.blade.php
@@ -535,52 +535,6 @@ form.addEventListener("submit",function(e){
 </form>
 </div>
 </div>
-
-<!-- FOOTER -->
-
-<div class="footer">
-
-<div class="footer-container">
-
-<div class="footer-col">
-<h3>Fil Products Eastern Visayas</h3>
-<p>
-Reliable Fiber Internet and Cable TV provider serving homes and businesses across Eastern Visayas.
-</p>
-</div>
-
-<div class="footer-col">
-<h4>Contact Us</h4>
-<p><i class="fas fa-phone"></i> 0917-320-5871</p>
-<p><i class="fas fa-phone"></i> 0938-320-5871</p>
-<p><i class="fas fa-envelope"></i> info.cyg@filproducts.ph</p>
-</div>
-
-<div class="footer-col">
-<h4>Visit Our Office</h4>
-<p>
-Bernate Compound, Capoocan<br>
-Calbayog City, Samar
-</p>
-<p>
-City Center Park Real St., Brgy Aslum<br>
-Tacloban City, Leyte
-</p>
-</div>
-
-<div class="footer-col">
-<h4>Follow Us</h4>
-
-<a href="https://www.facebook.com/FilProductsSamar/" target="_blank" class="social-link">
-<i class="fab fa-facebook"></i> Fil Products Samar
-</a>
-<a href="https://www.facebook.com/FilProductsLeyte/" target="_blank" class="social-link">
-<i class="fab fa-facebook"></i> Fil Products Leyte
-</a>
-
-</div>
-
-</div>
 @include('layouts.footer') 
 
 @endsection
