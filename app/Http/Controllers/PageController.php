c9073b38caec3f9456258b9a3cbe77ba4a047269
Hp <aljonrojas01@gmail.com>
2026-04-16 08:52:36 +0800

    new

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -179,7 +179,7 @@ public function submitComplaint(Request $request)
             'account_name' => $name,
             'date_created' => now()->toDateTimeString(),
             'address' => $address,
-            'fieldjob_type' => strip_tags($remarks),
+            'fieldjob_type' => $remarks,
             'prepared_by' => 'Website',
             'team_deployed' => '',
             'date_completed' => ''
